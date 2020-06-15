e266968ee512fa9da059df0d47b9c58dc620feb2
Mika Kaukolinna <mika.kaukolinna@gofore.com>
2020-06-16 01:03:27 +0300

    11

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -5,14 +5,44 @@ provider "aws" {
 }
 
 
+#terraform {
+#  backend "remote" {
+#    organization = "g-cloud-academy"
+#    hostname = "app.terraform.io"
+#    workspaces {
+#      name = "terraform-test"
+#    }
+#    token = var.tf_token
+#  }
+#}
+
 terraform {
-  backend "remote" {
-    organization = "g-cloud-academy"
-    hostname = "app.terraform.io"
-    workspaces {
-      name = "terraform-test"
-    }
-    token = var.tf_token
+  backend "s3" {
+    key = "statefile.tfstate"
+    bucket = var.bucket_name
+    dynamodb_table = var.table_name
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = var.bucket_name
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
 
--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
-variable "tf_token" {
+#variable "tf_token" {
+#  type    = string
+#}
+
+variable "bucket_name" {
   type    = string
+  default = "g-cloud-academy-terraform-backend"
+}
+
+variable "table_name" {
+  type    = string
+  default = "terraform-state-locking"
 }
